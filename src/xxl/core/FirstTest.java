a070b51cd441a3f7020be0995d27f33197ef9da9
n0xiv4 <rodrigospascoal@hotmail.com>
2023-10-08 01:37:59 +0100

    Tested serializable... it works!

--- src/xxl/core/FirstTest.java
+++ src/xxl/core/FirstTest.java
@@ -7,12 +7,16 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import xxl.core.exception.ImportFileException;
+import xxl.core.exception.MissingFileAssociationException;
+import xxl.core.exception.UnavailableFileException;
 
 public class FirstTest {
-	public static void main (String[] args) throws ImportFileException, FileNotFoundException, IOException {
+	public static void main (String[] args) throws ImportFileException, FileNotFoundException, IOException, MissingFileAssociationException, UnavailableFileException {
 		Calculator calculator = new Calculator();
-		calculator.importFile("D:\\Utilizadores\\Rodrigo\\Documentos\\OneDrive - IST\\OneDrive - Universidade de Lisboa\\Documentos\\2º Ano\\1º Semestre\\PO\\ProjetoGIT\\spreadsheet-po-proj\\bin\\xxl\\core\\test.txt");
-		
+		// calculator.importFile("D:\\Utilizadores\\Rodrigo\\Documentos\\OneDrive - IST\\OneDrive - Universidade de Lisboa\\Documentos\\2º Ano\\1º Semestre\\PO\\ProjetoGIT\\spreadsheet-po-proj\\bin\\xxl\\core\\test.txt");
+
+		calculator.load("xxl\\core\\folha1.ser");
+
 		Spreadsheet folha = calculator.getSpreadsheet();
 
 		System.out.println(folha.getValueInPosition(new Position(1, 1)));
